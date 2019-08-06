1ae81e2c8de3d07544da21c9aeafc1129efaaa7c
Francisco Souza <fsouza@users.noreply.github.com>
2019-08-05 22:20:12 -0400

    examples/python: simplify code and dependencies

--- examples/python/python.py
+++ examples/python/python.py
@@ -2,24 +2,34 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-from google.cloud import storage
-from google.auth.credentials import AnonymousCredentials
 import requests
 import urllib3
+from google.auth.credentials import AnonymousCredentials
+from google.cloud import storage
 
 BASE_URL = "https://127.0.0.1:4443"
 
-storage._http.Connection.API_BASE_URL = BASE_URL # override the BASE_URL in the client library with the mock server
-storage.blob._DOWNLOAD_URL_TEMPLATE = (u"%s/download/storage/v1{path}?alt=media" % BASE_URL)
-storage.blob._BASE_UPLOAD_TEMPLATE = (u"%s/upload/storage/v1{bucket_path}/o?uploadType=" % BASE_URL)
+storage._http.Connection.API_BASE_URL = (
+    BASE_URL
+)  # override the BASE_URL in the client library with the mock server
+storage.blob._DOWNLOAD_URL_TEMPLATE = (
+    u"%s/download/storage/v1{path}?alt=media" % BASE_URL
+)
+storage.blob._BASE_UPLOAD_TEMPLATE = (
+    u"%s/upload/storage/v1{bucket_path}/o?uploadType=" % BASE_URL
+)
 storage.blob._MULTIPART_URL_TEMPLATE = storage.blob._BASE_UPLOAD_TEMPLATE + u"multipart"
 storage.blob._RESUMABLE_URL_TEMPLATE = storage.blob._BASE_UPLOAD_TEMPLATE + u"resumable"
 
 my_http = requests.Session()
 my_http.verify = False  # disable SSL validation
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) # disable https warnings for https insecure certs
+urllib3.disable_warnings(
+    urllib3.exceptions.InsecureRequestWarning
+)  # disable https warnings for https insecure certs
 
-client = storage.Client(credentials=AnonymousCredentials(), project="test", _http=my_http)
+client = storage.Client(
+    credentials=AnonymousCredentials(), project="test", _http=my_http
+)
 
 # List the Buckets
 for bucket in client.list_buckets():
