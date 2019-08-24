4bbf7ba52e055aa63951360cf8de09dc8baa3daa
francisco souza <fsouza@users.noreply.github.com>
2019-08-24 16:39:55 -0700

    ci/run-python-example: fix server

--- ci/run-python-example.sh
+++ ci/run-python-example.sh
@@ -2,7 +2,7 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-./fake-gcs-server &
+./fake-gcs-server -backend memory -data $PWD/examples/data &
 
 EXTERNAL_URL=https://localhost:4443
 PUBLIC_HOST=https://localhost:4443
