64c4f3108c33d4589bca4720cfe0ead6415ee6bf
francisco souza <fsouza@users.noreply.github.com>
2020-04-20 17:19:33 -0400

    ci: exit on failure
    wat

--- ci/run-go-example.sh
+++ ci/run-go-example.sh
@@ -2,6 +2,8 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
+set -e
+
 ./fake-gcs-server -backend memory -data $PWD/examples/data -public-host storage.gcs.127.0.0.1.nip.io:4443 &
 
 (
