c0ab8bf544622ceda3a11d2043693fe774e92c9b
kennytm <kennytm@gmail.com>
2020-04-30 06:30:58 +0800

    Fix gosec G306 error

--- internal/backend/fs.go
+++ internal/backend/fs.go
@@ -122,7 +122,7 @@ func (s *storageFS) CreateObject(obj Object) error {
 	if err != nil {
 		return err
 	}
-	return ioutil.WriteFile(filepath.Join(s.rootDir, url.PathEscape(obj.BucketName), url.PathEscape(obj.Name)), encoded, 0664)
+	return ioutil.WriteFile(filepath.Join(s.rootDir, url.PathEscape(obj.BucketName), url.PathEscape(obj.Name)), encoded, 0600)
 }
 
 // ListObjects lists the objects in a given bucket with a given prefix and
