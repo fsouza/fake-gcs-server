118187c4fac77516f971ed8a517b12d47ce46b36
francisco souza <fsouza@users.noreply.github.com>
2022-01-12 23:29:18 -0500

    Run scala example in CI

--- ci/run-scala-example.sh
+++ ci/run-scala-example.sh
@@ -1,8 +1,12 @@
+# Copyright 2022 Francisco Souza. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
 set -e
 
 ./fake-gcs-server -backend memory -scheme http -port 8080 -external-url "http://localhost:8080" &
 
 (
-    cd examples/scala
-    sbt run
+	cd examples/scala
+	sbt run
 )
