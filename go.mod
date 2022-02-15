0d523fe7c3677a92dc0bab97ee97777a91588d58
Andrew Gaul <gaul@google.com>
2022-02-15 22:54:44 +0900

    Store object data natively and metadata in xattr (#683)
    The latter remains a JSON-encoded blob but is now stored in file
    extended attributes, except on Windows where it is a separate file.
    This reduces memory usage and is much faster by avoiding JSON-encoding
    large objects.  This enables a future commit to avoid reading the
    entire object, particularly for range requests.  Note that this commit
    changes the on-disk format and is not compatible with previous data
    sets.  Extended attributes have some caveats including lack of tmpfs
    and Windows support.  References pkg/xattr#47.  References #669.
    Fixes #671.

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/google/go-cmp v0.5.7
 	github.com/gorilla/handlers v1.5.1
 	github.com/gorilla/mux v1.8.0
+	github.com/pkg/xattr v0.4.5
 	github.com/sirupsen/logrus v1.8.1
 	github.com/stretchr/testify v1.7.0
 	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8
---
+++ internal/backend/xattr_unix.go
@@ -0,0 +1,30 @@
+// Copyright 2017 Francisco Souza. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build !windows
+// +build !windows
+
+package backend
+
+import (
+	"github.com/pkg/xattr"
+)
+
+const xattrKey = "user.metadata"
+
+func writeXattr(path string, encoded []byte) error {
+	return xattr.Set(path, xattrKey, encoded)
+}
+
+func readXattr(path string) ([]byte, error) {
+	return xattr.Get(path, xattrKey)
+}
+
+func isXattrFile(path string) bool {
+	return false
+}
+
+func removeXattrFile(path string) error {
+	return nil
+}
