7f00251eda1f39b0df25ab33f4bf64eac41d55b5
MRG FOSS <mrg.foss@gmail.com>
2024-03-10 07:35:32 +0100

    Fill the `errors` attribute for HTTP errors
    Some GCS client libraries (like [this one][1]), are quite strict when it
    comes to the set of attributes they expect to find in the error
    response. If any of those attributes is missing, they fail to
    deserialize the error.
    
    In `fake-gcs-server`, the `httpError.Errors` attribute was always
    initialized to `nil`, causing the `error.errors` JSON response attribute
    to be omitted and leading to failures in some client libraries.
    
    In this commit we address that issue by always providing content on the
    `errors` attribute (`httpError.Errors` field) whenever we return an
    error response. The implementation is kept simple without aiming to
    return exactly the same values that are [officially documented][2]. The
    main goal is to have a sensible response that is _structurally_ correct
    for the purposes of deserialization client-side.
    
    [1]: https://crates.io/crates/google-cloud-storage
    [2]: https://cloud.google.com/storage/docs/json_api/v1/status-codes

--- fakestorage/object_test.go
+++ fakestorage/object_test.go
@@ -2028,7 +2028,7 @@ func TestServiceClientComposeObject(t *testing.T) {
 				"files/destination.txt",
 				[]string{"01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33"},
 				"",
-				"googleapi: Error 400: The number of source components provided (33) exceeds the maximum (32)",
+				"googleapi: Error 400: The number of source components provided (33) exceeds the maximum (32), Bad Request",
 			},
 		}
 		for _, test := range tests {
